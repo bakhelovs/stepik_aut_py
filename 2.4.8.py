2774825dbc2db60a9bb4b451a8e823a612105f7a
bakhelovs <sespace@ya.ru>
2020-12-11 17:22:25 +0300

    clear some code

--- 2.4.8.py
+++ 2.4.8.py
@@ -8,9 +8,6 @@ browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
 #browser.implicitly_wait(12)
 
-#browser.find_element_by_css_selector("button#book.btn.btn-primary").click()
-
-
 button = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID,"price"),"$100"))
 browser.find_element_by_css_selector("button#book.btn.btn-primary").click()
 browser.execute_script("window.scrollBy(0, 100);")
@@ -19,6 +16,6 @@ y = str(math.log(abs(12*math.sin(int(x)))))
 browser.find_element_by_id("answer").send_keys(y)
 browser.find_element_by_id("solve").click()
 
-#browser.quit()
+browser.quit()
 
 
